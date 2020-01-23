889ffd9ee59ab3c9a944257ed21382ac481332e4
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-01-23 14:15:58 +0800

    Add TodoInput

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoList from "../components/TodoList";
+import TodoInput from "../components/TodoInput";
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <TodoInput />
         <TodoList />
       </div>
     );
---
+++ src/components/TodoInput.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TodoInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <button>Add todo</button>
+      </div>
+    );
+  }
+}
+
+export default TodoInput;
