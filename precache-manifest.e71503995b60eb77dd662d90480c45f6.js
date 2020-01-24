0dfdcae0b42cf20ef06cc2a7af9f89e85039cdb1
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-01-24 08:40:43 +0800

    Add built files

---
+++ precache-manifest.e71503995b60eb77dd662d90480c45f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b55e2e0d8dad54f0767a01db84a2b8b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "86dc1307168bdda8f7cd",
+    "url": "/react-todo/static/css/main.d8553cd2.chunk.css"
+  },
+  {
+    "revision": "2c0ea8141b617998ca00",
+    "url": "/react-todo/static/js/2.ebe16367.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.ebe16367.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86dc1307168bdda8f7cd",
+    "url": "/react-todo/static/js/main.b2fc2ac1.chunk.js"
+  },
+  {
+    "revision": "4637a77acbbb1bfa3441",
+    "url": "/react-todo/static/js/runtime-main.852ef8c9.js"
+  }
+]);
